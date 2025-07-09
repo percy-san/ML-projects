1d3b11932af9debe5f7f0a07b170de4d0affe8df
Buhlebethu Mkhonta <percymkhonta@gmail.com>
2025-07-09 20:34:59 +0200

    EDA & Problem statment

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 
 def error_message_details(error, error_detail):
@@ -21,3 +22,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
