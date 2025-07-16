dcf75e79e1a613bffb41dd72b886c6960fb1b990
Buhlebethu Mkhonta <percymkhonta@gmail.com>
2025-07-16 19:19:26 +0200

    Prediction Pipline

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill  # dill is like pickle but can handle more complex Python objects
+import pickle
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
@@ -59,5 +60,14 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
